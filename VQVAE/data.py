05dda3d6e73d9d16fe49604bb0f479f1e6b070a3
Haiyun Huang <81820136+fleckleind@users.noreply.github.com>
2025-03-27 15:45:57 +0800

    Create data.py

---
+++ VQVAE/data.py
@@ -0,0 +1,23 @@
+from PIL import Image
+from torchvision import transforms
+from torch.utils.data import Dataset
+
+
+class CelebADataset(Dataset):
+    def __init__(self, root, img_shape=(64, 64)):
+        super().__init__()
+        self.root = root
+        self.img_shape = img_shape
+        self.filenames = sorted(os.listdir(root))
+
+    def __len__(self) -> int:
+        return len(self.filenames)
+
+    def __getitem__(self, index: int):
+        path = os.path.join(self.root, self.filenames[index])
+        img = Image.open(path)
+        pipeline = transforms.Compose([
+            transforms.CenterCrop(168),
+            transforms.Resize(self.img_shape),
+            transforms.ToTensor()])
+        return pipeline(img)
