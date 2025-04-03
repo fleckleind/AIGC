ac5362bd550116e41632166fdfe3f74e49d182c4
Haiyun Huang <81820136+fleckleind@users.noreply.github.com>
2025-04-03 16:38:15 +0800

    Update model.py

--- VQGAN/model.py
+++ VQGAN/model.py
@@ -1,1 +1,249 @@
+# VQ-GAN
+import torch
 import torch.nn as nn
+import torch.nn.functional as F
+
+
+class GroupNorm(nn.Module):
+    def __init__(self, channels):
+        super(GroupNorm, self).__init__()
+        self.gn = nn.GroupNorm(num_groups=32, num_channels=channels, eps=1e-6, affine=True)
+
+    def forward(self, x):
+        return self.gn(x)
+
+
+class Swish(nn.Module):
+    def forward(self, x):
+        return x * torch.sigmoid(x)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock, self).__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.block = nn.Sequential(
+            GroupNorm(in_channels),
+            Swish(),
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1),
+            GroupNorm(out_channels),
+            Swish(),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1)
+        )  # (groupnorm->sigmoid->conv)*2
+
+        if in_channels != out_channels:  # adjust channel size
+            self.channel_up = nn.Conv2d(in_channels, out_channels, 1, 1, 0)
+
+    def forward(self, x):
+        if self.in_channels != self.out_channels:
+            return self.channel_up(x) + self.block(x)
+        else:
+            return x + self.block(x)
+
+
+class UpSampleBlock(nn.Module):
+    def __init__(self, channels):
+        super(UpSampleBlock, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, 3, 1, 1)
+
+    def forward(self, x):
+        x = F.interpolate(x, scale_factor=2.0)
+        return self.conv(x)
+
+
+class DownSampleBlock(nn.Module):
+    def __init__(self, channels):
+        super(DownSampleBlock, self).__init__()
+        self.conv = nn.Conv2d(channels, channels, 3, 2, 0)
+
+    def forward(self, x):
+        pad = (0, 1, 0, 1)
+        x = F.pad(x, pad, mode="constant", value=0)
+        return self.conv(x)
+
+
+class NonLocalBlock(nn.Module):
+    def __init__(self, channels):
+        super(NonLocalBlock, self).__init__()
+        self.in_channels = channels
+
+        self.gn = GroupNorm(channels)
+        self.q = nn.Conv2d(channels, channels, 1, 1, 0)
+        self.k = nn.Conv2d(channels, channels, 1, 1, 0)
+        self.v = nn.Conv2d(channels, channels, 1, 1, 0)
+        self.proj_out = nn.Conv2d(channels, channels, 1, 1, 0)
+
+    def forward(self, x):
+        h_ = self.gn(x)
+        q = self.q(h_)
+        k = self.k(h_)
+        v = self.v(h_)
+
+        b, c, h, w = q.shape
+
+        q = q.reshape(b, c, h*w)
+        q = q.permute(0, 2, 1)
+        k = k.reshape(b, c, h*w)
+        v = v.reshape(b, c, h*w)
+
+        attn = torch.bmm(q, k)
+        attn = attn * (int(c)**(-0.5))
+        attn = F.softmax(attn, dim=2)
+        attn = attn.permute(0, 2, 1)
+
+        A = torch.bmm(v, attn)
+        A = A.reshape(b, c, h, w)
+
+        return x + A
+
+
+class Encoder(nn.Module):
+    def __init__(self, args):
+        super(Encoder, self).__init__()
+        channels = [128, 128, 128, 256, 256, 512]
+        attn_resolutions = [16]
+        num_res_blocks = 2
+        resolution = 256
+        layers = [nn.Conv2d(args.image_channels, channels[0], 3, 1, 1)]  # layer[0]
+        for i in range(len(channels)-1):  # layer[1,5]
+            in_channels = channels[i]
+            out_channels = channels[i + 1]
+            for j in range(num_res_blocks):  # 2 sucesstive residual block
+                layers.append(ResidualBlock(in_channels, out_channels))
+                in_channels = out_channels
+                if resolution in attn_resolutions:  # last layer
+                    layers.append(NonLocalBlock(in_channels))
+            if i != len(channels)-2:  # downsample layer: 1, 2, 3, 5
+                layers.append(DownSampleBlock(channels[i+1]))  # conv stride
+                resolution //= 2
+        # bottleneck layer
+        layers.append(ResidualBlock(channels[-1], channels[-1]))
+        layers.append(NonLocalBlock(channels[-1]))
+        layers.append(ResidualBlock(channels[-1], channels[-1]))
+        layers.append(GroupNorm(channels[-1]))
+        layers.append(Swish())  # sigmoid
+        # encoder output before codebook
+        layers.append(nn.Conv2d(channels[-1], args.latent_dim, 3, 1, 1))
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Decoder(nn.Module):
+    def __init__(self, args):
+        super(Decoder, self).__init__()
+        channels = [512, 256, 256, 128, 128]
+        attn_resolutions = [16]
+        num_res_blocks = 3
+        resolution = 16
+
+        in_channels = channels[0]
+        layers = [nn.Conv2d(args.latent_dim, in_channels, 3, 1, 1),
+                  ResidualBlock(in_channels, in_channels),
+                  NonLocalBlock(in_channels),  # attention
+                  ResidualBlock(in_channels, in_channels)]  # after codebook, layer[0]
+
+        for i in range(len(channels)):
+            out_channels = channels[i]
+            for j in range(num_res_blocks):  # 3 successive residual blocks
+                layers.append(ResidualBlock(in_channels, out_channels))
+                in_channels = out_channels
+                if resolution in attn_resolutions:  # layer[1], attention
+                    layers.append(NonLocalBlock(in_channels))
+            if i != 0:  # up: layer[2,3,4,5]
+                layers.append(UpSampleBlock(in_channels))  # interpolate
+                resolution *= 2
+
+        layers.append(GroupNorm(in_channels))
+        layers.append(Swish())  # sigmoid
+        layers.append(nn.Conv2d(in_channels, args.image_channels, 3, 1, 1))
+        self.model = nn.Sequential(*layers)  # output
+
+    def forward(self, x):
+        return self.model(x)
+
+
+class Codebook(nn.Module):
+    def __init__(self, args):
+        super(Codebook, self).__init__()
+        self.num_codebook_vectors = args.num_codebook_vectors
+        self.latent_dim = args.latent_dim
+        self.beta = args.beta
+
+        self.embedding = nn.Embedding(self.num_codebook_vectors, self.latent_dim)
+        self.embedding.weight.data.uniform_(-1.0 / self.num_codebook_vectors, 1.0 / self.num_codebook_vectors)
+
+    def forward(self, z):
+        z = z.permute(0, 2, 3, 1).contiguous()  # (b,c,h,w)->(b,h,w,c)
+        z_flattened = z.view(-1, self.latent_dim)  # (b*h*w*c/self.latent_dim, self.latent_dim)
+
+        # distance: ||z_e||^2+||e_j||^2-2z_e\cdot e_j
+        d = torch.sum(z_flattened**2, dim=1, keepdim=True) + \
+            torch.sum(self.embedding.weight**2, dim=1) - \
+            2*(torch.matmul(z_flattened, self.embedding.weight.t()))  
+
+        min_encoding_indices = torch.argmin(d, dim=1)  # nearest neighbour selection
+        z_q = self.embedding(min_encoding_indices).view(z.shape)  # get z_q, (b,h,w,c)
+
+        # commitment loss and attentive loss
+        loss = torch.mean((z_q.detach() - z)**2) + self.beta * torch.mean((z_q - z.detach())**2)
+        z_q = z + (z_q - z).detach()  # straight-through estimator: propagate gradient
+        
+        z_q = z_q.permute(0, 3, 1, 2)  # decoder input dimension
+        return z_q, min_encoding_indices, loss
+
+
+class VQGAN(nn.Module):
+    def __init__(self, args):
+        super(VQGAN, self).__init__()
+        self.encoder = Encoder(args).to(device=args.device)
+        self.decoder = Decoder(args).to(device=args.device)
+        self.codebook = Codebook(args).to(device=args.device)
+        # codebook transferation, before and after vector-quantisation
+        self.quant_conv = nn.Conv2d(args.latent_dim, args.latent_dim, 1).to(device=args.device)
+        self.post_quant_conv = nn.Conv2d(args.latent_dim, args.latent_dim, 1).to(device=args.device)
+
+    def forward(self, imgs):
+        encoded_images = self.encoder(imgs)  # z_e
+        quant_conv_encoded_images = self.quant_conv(encoded_images)  # preprocess z_e
+        # codebook, vector quantisation: v_q, v_q indices, vq-loss(reconsturction+codebook)
+        codebook_mapping, codebook_indices, q_loss = self.codebook(quant_conv_encoded_images)
+        post_quant_conv_mapping = self.post_quant_conv(codebook_mapping)  # postprocess z_q
+        decoded_images = self.decoder(post_quant_conv_mapping)  # \hat{x}
+        return decoded_images, codebook_indices, q_loss
+
+    def encode(self, imgs):
+        encoded_images = self.encoder(imgs)
+        quant_conv_encoded_images = self.quant_conv(encoded_images)
+        codebook_mapping, codebook_indices, q_loss = self.codebook(quant_conv_encoded_images)
+        return codebook_mapping, codebook_indices, q_loss
+
+    def decode(self, z):
+        post_quant_conv_mapping = self.post_quant_conv(z)
+        decoded_images = self.decoder(post_quant_conv_mapping)
+        return decoded_images
+
+    def calculate_lambda(self, perceptual_loss, gan_loss):
+        # adaptive loss weighting
+        # last layer for better to advance quality of synthesic image
+        last_layer = self.decoder.model[-1]
+        last_layer_weight = last_layer.weight
+        # auto-gradient calculation for last layer based on 2 losses
+        perceptual_loss_grads = torch.autograd.grad(perceptual_loss, last_layer_weight, retain_graph=True)[0]
+        gan_loss_grads = torch.autograd.grad(gan_loss, last_layer_weight, retain_graph=True)[0]
+        # calculate the gradient normalization, to balance the optimization between perceptual and GAN
+        λ = torch.norm(perceptual_loss_grads) / (torch.norm(gan_loss_grads) + 1e-4)
+        λ = torch.clamp(λ, 0, 1e4).detach()  # filter out outliers outside [0, 1e-4]
+        return 0.8 * λ
+
+    @staticmethod
+    def adopt_weight(disc_factor, i, threshold, value=0.):
+        # adopt weight coefficient if epoch-i < threshold
+        if i < threshold:
+            disc_factor = value
+        return disc_factor
+
+    def load_checkpoint(self, path):
+        self.load_state_dict(torch.load(path))
